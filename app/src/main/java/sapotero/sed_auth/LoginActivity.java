729f8e4242d60dab07a032c0534b229495a66a95
Сапожников Егор <esapozhnikov@n-core.ru>
2016-09-01 12:32:12 +0300

    Add uniq id generation on every request && Auth dialog && Simple service

--- app/src/main/java/sapotero/sed_auth/LoginActivity.java
+++ app/src/main/java/sapotero/sed_auth/LoginActivity.java
@@ -1,9 +1,11 @@
 package sapotero.sed_auth;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,47 +17,88 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.UUID;
+
 public class LoginActivity extends AppCompatActivity {
 
-    public final static String EXTRA_MESSAGE = "MESSAGE";
-    public final static String TOKEN_URL = "http://mobile.esd.n-core.ru/token/";
+  public final static String EXTRA_MESSAGE = "MESSAGE";
+  public final static String HOST = "http://mobile.esd.n-core.ru/";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+  public final static String TOKEN_URL  = "http://mobile.esd.n-core.ru/token/";
+  public final static String REPORT_URL = "/v3/documents.json?status_code=sent_to_the_report";
+  public ProgressDialog progressDialog;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+  }
+
+  public void tryToLogin(View view) {
+
+    EditText username = (EditText) findViewById(R.id.input_username);
+    final String usernameValue = username.getText().toString();
+
+    EditText password = (EditText) findViewById(R.id.input_password);
+    final String passwordValue= password.getText().toString();
+
+    progressDialog = new ProgressDialog(LoginActivity.this);
+    progressDialog.setIndeterminate(true);
+    progressDialog.setMessage("Authenticating...");
+    progressDialog.show();
+
+    new android.os.Handler().postDelayed(
+      new Runnable() {
+        public void run() {
+          sendLoginRequest( usernameValue, passwordValue );
+        }
+      }, 3000);
+
+  }
+
+  public void sendLoginRequest( String username, String password){
+    final Intent intent = new Intent(this, UserActivity.class);
+    final Context context= this;
+
+    RequestQueue queue = Volley.newRequestQueue(this);
+
+    // Request a string response from the provided URL.
+    StringRequest stringRequest = new StringRequest(Request.Method.PUT, TOKEN_URL+String.format("%s.json?password=%s", username, password),
+        new Response.Listener<String>() {
+          @Override
+          public void onResponse(String response) {
+            progressDialog.dismiss();
+            intent.putExtra( EXTRA_MESSAGE, response );
+            startActivity(intent);
+            onLoginSuccess();
+          }
+        }, new Response.ErrorListener() {
+
+      @Override
+      public void onErrorResponse(VolleyError error) {
+        progressDialog.dismiss();
+        Toast.makeText( context, "That didn't work!", Toast.LENGTH_SHORT).show();
+      }
+    });
+    queue.add(stringRequest);
+  }
+
+  public void onLoginSuccess() {
+    finish();
+  }
+
+  public static String getUniqueID() {
+    String m_szDevIDShort = "35" + (Build.BOARD.length() % 10) + (Build.BRAND.length() % 10) + (Build.CPU_ABI.length() % 10) + (Build.DEVICE.length() % 10) + (Build.MANUFACTURER.length() % 10) + (Build.MODEL.length() % 10) + (Build.PRODUCT.length() % 10);
+
+    String serial = null;
+    try {
+      serial = android.os.Build.class.getField("SERIAL").get(null).toString();
+
+      return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
+    } catch (Exception exception) {
+      serial = "serial"; // some value
     }
 
-    public void tryToLogin(View view) {
-        final Intent intent = new Intent(this, UserActivity.class);
-        final Context context= this;
-
-        EditText username = (EditText) findViewById(R.id.input_username);
-        String usernameValue = username.getText().toString();
-
-        EditText password = (EditText) findViewById(R.id.input_password);
-        String passwordValue= password.getText().toString();
-
-
-        RequestQueue queue = Volley.newRequestQueue(this);
-
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.PUT, TOKEN_URL+String.format("%s.json?password=%s", usernameValue, passwordValue),
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        intent.putExtra( EXTRA_MESSAGE, response.toString() );
-                        startActivity(intent);
-                    }
-                }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-//                intent.putExtra(EXTRA_MESSAGE, "That didn't work!");
-//                startActivity(intent);
-                Toast.makeText( context, "That didn't work!", Toast.LENGTH_SHORT).show();
-            }
-        });
-        queue.add(stringRequest);
-    }
+    return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
+  }
 }
--- app/src/main/java/sapotero/sed_auth/UserActivity.java
+++ app/src/main/java/sapotero/sed_auth/UserActivity.java
@@ -1,29 +1,71 @@
 package sapotero.sed_auth;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class UserActivity extends AppCompatActivity {
-    public final static String TOKEN = "";
+  public final static String TOKEN = "";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
 
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(LoginActivity.EXTRA_MESSAGE);
-        System.out.println( message );
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_user);
 
-        TextView data = (TextView)findViewById(R.id.data);
-        data.setText( message );
+    Intent intent = getIntent();
+    String message = intent.getStringExtra(LoginActivity.EXTRA_MESSAGE);
+    System.out.println( message );
 
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-
+    TextView data = (TextView)findViewById(R.id.data);
+    data.setText( message );
 
 
+    message = getMd5Hash( SystemClock.currentThreadTimeMillis() + LoginActivity.getUniqueID() );
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+  }
+
+  @Override
+  public void onBackPressed() {
+    super.moveTaskToBack(true);
+  }
+
+  @Override
+  protected void onUserLeaveHint() {
+    Toast toast = Toast.makeText(getApplicationContext(), "Нажата кнопка HOME", Toast.LENGTH_SHORT);
+    toast.show();
+    super.onUserLeaveHint();
+  }
+
+  public static String getMd5Hash(String input) {
+    try {
+      MessageDigest md = MessageDigest.getInstance("MD5");
+      byte[] messageDigest = md.digest(input.getBytes());
+      BigInteger number = new BigInteger(1, messageDigest);
+      String md5 = number.toString(16);
+
+      while (md5.length() < 32)
+        md5 = "0" + md5;
+
+      return md5;
+    } catch (NoSuchAlgorithmException e) {
+      Log.e("MD5", e.getLocalizedMessage());
+      return null;
     }
+  }
+
+  public void logUniqId(View view) {
+    String message = getMd5Hash( System.nanoTime() + LoginActivity.getUniqueID() );
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+  }
+
 }
